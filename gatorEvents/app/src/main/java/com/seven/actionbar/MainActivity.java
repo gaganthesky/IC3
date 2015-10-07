b8cc9fc7ecaa43e8ec946ddf00c9f78ec8601260
ZhaoheXu1993 <zhaohexu1993@gmail.com>
2015-10-07 01:51:09 -0400

    Update
    position = 2, wrong

--- gatorEvents/app/src/main/java/com/seven/actionbar/MainActivity.java
+++ gatorEvents/app/src/main/java/com/seven/actionbar/MainActivity.java
@@ -1,13 +1,14 @@
 package com.seven.actionbar;
 
 import android.app.Activity;
-
+import android.content.Intent;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,6 +18,7 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -53,6 +55,29 @@ public class MainActivity extends Activity
         fragmentManager.beginTransaction()
                 .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
                 .commit();
+        /*if(position==0)
+        {
+            try {
+                setContentView(R.layout.activity_main2);
+            }
+            catch(IllegalArgumentException e){
+                throw(e);
+            }
+
+        }*/
+
+       /* if(position==2) {
+            this.findViewById(R.id.action_search).setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                        public void onClick(View v)
+                {
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this, Main2Activity.class);
+                    startActivity(intent);
+                }
+            });
+        }*/
     }
 
     @Override
@@ -132,9 +157,10 @@ public class MainActivity extends Activity
         }
 
         @Override
-            public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                      Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
             return rootView;
         }
 
