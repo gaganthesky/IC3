44c6f7204b239318a2304c4ec115951c9436b980
gaganthesky <gaganthesky83@gmail.com>
2015-10-11 19:04:27 -0400

    Web Sites Updated, Data Synchronization active
    All web-app files from Alan and Change are updates

---
+++ Web part/Final/registration.php
@@ -0,0 +1,50 @@
+<?php
+
+
+$servername = 	"mysql-gatorevents-db.ufgatorevents.com";
+$username = 	"gatorevents_db";
+$password = 	"ic3_ZVSGCA";
+
+// Create connection
+$conn = mysql_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " .  $conn->connect_error);
+} 
+echo "Connected successfully";
+
+//define('db_name, 'gatorevents_db');
+$db_selected = mysql_select_db(gatorevents_db, $conn);
+
+if(!$db_selected)
+{
+	die('Can not use' . gatorevents_db . ': ' . mysql_errno());
+}
+
+$value1 = $_POST['event_name'];
+$value2 = $_POST['date'];
+$value3 = $_POST['organization'];
+$value4 = $_POST['location'];
+$value5 = $_POST['event_details'];
+
+$sql = "INSERT INTO events_temp (event_name, date, organization, location, event_details) VALUES ('$value1', '$value2', '$value3', '$value4', '$value5')";
+//$sql = "INSERT INTO events_temp (date, organization, location, event_details) VALUES ('$value2', '$value3', '$value4', '$value5')";
+//$sql = "INSERT INTO events_temp (event_name, date, organization, location, event_details) VALUES ('Gator Event', '2015-10-10', 'UFL', 'CISE E-213', 'A weekly workshop ')";
+
+if(!mysql_query($sql))
+{
+	die('Error: '. mysql_error());
+}
+
+mysql_close();
+
+
+?>
+
+
+
+
+
+
+
