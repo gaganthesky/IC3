51d2eb62aa00023c51feb187a722ad85ac728d49
ZhaoheXu1993 <zhaohexu1993@gmail.com>
2015-10-20 23:51:14 -0400

    The php files
    Open database connection & receive the json to create user

---
+++ php/db_config.php
@@ -0,0 +1,9 @@
+<?php
+/*All database connection variables*/
+
+define('DB_USER',"gatorevents_db");
+define('DB_PASSWORD',"ic3_ZVSGCA");
+define('DB_DATABASE',"gatorevents_db");
+define('DB_SERVER',"mysql-gatorevents-db.ufgatorevents.com");
+
+?>
